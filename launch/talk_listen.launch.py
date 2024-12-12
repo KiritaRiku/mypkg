2ba4b1676c54c4abb55f026930a26299b179c9ff
Kirita Riku <rikuribo1128@icloud.com>
2024-12-12 12:06:54 +0900

    add lesson10

--- launch/talk_listen.launch.py
+++ launch/talk_listen.launch.py
@@ -9,11 +9,11 @@ def generate_launch_description():
     talker = launch_ros.actions.Node(
         package='mypkg',
         executable='talker',
+        output='screen'
         )
     listener = launch_ros.actions.Node(
             package='mypkg',
             executable='listener',
             output='screen'
             )
-
-    return launch.LaunchDescription([talker, listener])
+    return launch.LaunchDescription([talker, listener, ])
