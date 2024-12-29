3d67c037ed8e258ae2365bc906fde2f6625d89a9
Kirita Riku <rikuribo1128@icloud.com>
2024-12-30 01:10:31 +0900

    12/30 3rdtest

--- test/test.bash
+++ test/test.bash
@@ -10,7 +10,23 @@ source install/setup.bash
 source install/local_setup.bash
 source $dir/.bashrc
 
+ng() {
+    echo "${1}行目が違う"
+    res=1
+}
+
+res=0
+
 timeout 10 ros2 launch mypkg timer_listen.launch.py > /tmp/mypkg.log
-cat /tmp/mypkg.log
-grep  "現在時刻:"
-grep  '経過時刻:1.00秒'
+
+out=$(cat /tmp/mypkg.log)
+
+echo grep -q "現在時刻:" "$out" || ng "$LINENO"
+echo grep -q '経過時刻:1.00秒' "$out" || ng "$LINENO"
+
+
+if [ "$res" = 0 ]; then
+    echo "OK"
+fi
+
+exit $res
