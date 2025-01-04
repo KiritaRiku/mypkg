ae07b507f528be53969f2050b9cbbbe20cbdb0e2
Kirita Riku <rikuribo1128@icloud.com>
2025-01-04 13:57:00 +0900

    add a README

--- test/test.bash
+++ test/test.bash
@@ -14,51 +14,40 @@ source $dir/.bashrc
 
 res=0
 
-# ノードをフォアグラウンドで実行
-echo "ノードのフォアグラウンド実行を開始します"
-ros2 run mypkg timer & # ノードをバックグラウンドで起動
+
+ros2 run mypkg timer &
 NODE_PID=$!
 
-# トピックの出力をログに保存
-echo "トピックの出力をログに保存します: /tmp/mypkg.log"
+
 ros2 topic echo /time_information std_msgs/msg/String >> /tmp/mypkg.log &
 TOPIC_PID=$!
 
-# 400秒間待機してトピックの出力を確保
-echo "350秒間の待機を開始します"
+echo "350秒の待機"
 sleep 350
 
-
-# 3分経過メッセージを確認
-echo "3分経過メッセージを確認します"
 if grep -q "3分経過しました!" /tmp/mypkg.log; then
-    echo "3分経過メッセージを確認しました"
+    echo "3分経過を確認"
 else
-    echo "3分経過メッセージが見つかりません"
+    echo "見つかりません"
     res=1
 fi
 
-# 5分経過メッセージを確認
-echo "5分経過メッセージを確認します"
+
 if grep -q "5分経過しました！ノードを停止します。" /tmp/mypkg.log; then
-    echo "5分経過メッセージを確認しました"
+    echo "5分経過を確認"
 else
-    echo "5分経過メッセージが見つかりません"
+    echo "見つかりません"
     res=1
 fi
 
-# ノードのシャットダウン
-echo "ノードをシャットダウンします"
+
 ros2 node list | grep -q mypkg.timer && ros2 node shutdown mypkg.timer
-echo "ノードがシャットダウンしました"
 
-# トピックのプロセスをシャットダウンします
-echo "トピックのプロセスをシャットダウンします"
+
 kill $TOPIC_PID
-echo "トピックのプロセスがシャットダウンしました"
 
-# 結果の確認
-if [ "$res" -eq 0 ]; then
+
+if [ "$res" = 0 ]; then
     echo "OK"
 else
     echo "エラーが発生しました"
